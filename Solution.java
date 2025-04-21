935e80d0ecf9d2e51030a511d4d8207236c8ed2e
hyun2y00 <pottq577@naver.com>
2025-04-21 16:35:30 +0900

    Feat: add Solution - test method

--- Solution.java
+++ Solution.java
@@ -3,4 +3,8 @@ public class Solution{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public static void test(){
+        System.out.println("do something");
+    }
 }
